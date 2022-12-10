8f18623e48e697d31f15ee85c5eea3985fe45513
Mingyu Lu <mingyulu@l2lambda.cs.washington.edu>
2022-12-09 16:04:25 -0800

    first commit

---
+++ experiment_fluid.py
@@ -0,0 +1,78 @@
+import numpy as np
+import pandas as pd
+import shap
+import os
+import sys
+import collections
+
+from sklearn.impute import SimpleImputer
+from sklearn import preprocessing
+
+module_path = os.path.abspath(os.path.join('CATENets/'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+from catenets.models.jax import TNet, SNet,SNet1, SNet2, SNet3, DRNet, RANet, PWNet, RNet, XNet
+
+
+#### filtering out procedure
+
+fluid_cohort = pd.read_pickle("data/low_bp_survival.pkl")
+
+fluid_cohort = fluid_cohort[fluid_cohort.columns.drop(list(fluid_cohort.filter(regex='proc')))]
+x_train = fluid_cohort.loc[:, ~fluid_cohort.columns.isin(["registryid","treated","outcome"])]
+
+### normalize x_train 
+#x = x_train.values 
+
+min_max_scaler = preprocessing.MinMaxScaler()
+x_train_scaled = min_max_scaler.fit_transform(x_train)
+
+imp = SimpleImputer(missing_values=np.nan, strategy='mean')
+imp.fit(x_train_scaled)
+x_train_scaled = imp.transform(x_train_scaled)
+
+n = x_train.shape[0]
+
+rng = np.random.default_rng(0)
+inds = np.arange(n)
+rng.shuffle(inds)
+
+n_train = int(0.8 * n)
+
+train_inds = inds[:n_train]
+test_inds = inds[n_train:]
+
+X_train, X_test = x_train_scaled[train_inds], x_train_scaled[test_inds]
+w_train, w_test = fluid_cohort["treated"].values[train_inds], fluid_cohort["treated"].values[test_inds]
+y_train, y_test = fluid_cohort["outcome"].values[train_inds], fluid_cohort["outcome"].values[test_inds]
+
+top_k_results = []
+
+models = [TNet(), DRNet(), RNet(),RANet(), PWNet(), XNet(), SNet() , SNet1(), SNet2(), SNet3()]
+
+#### Getting top 10 features from multiple runs. 
+
+names = x_train.columns
+seeds = np.arange(0, 6, 0.5, dtype=int)
+
+for i in seeds:
+    np.random.seed(i)
+
+    for model in models:
+        model.fit(X_train, y_train, w_train)
+        model_lam = lambda x: model.predicr(x)
+
+        explainer = shap.Explainer(model_lam, X_train)
+        #### showing explanation on cate
+        shap_values = explainer(X_test)
+        ind = np.argpartition(np.abs(shap_values.values).mean(0).round(2), -10)[-10:]
+        top_k_results.extend(names[ind].tolist())
+
+### TODO: calcualte CIs across different seeds
+
+results = collections.Counter(top_k_results)
+summary = pd.DataFrame(results.items(), columns=['feature', 'count (%)']).sort_values(by="count (%)", ascending=False)
+summary["count (%)"] = np.round(summary["count (%)"]/(len(models)*len(seeds)),2)*100
+
+summary.to_csv("results/top_10_fatures.csv")
---
+++ experiment_missingness.py
@@ -0,0 +1,118 @@
+import os
+import sys
+import xgboost
+import shap
+import argparse
+import pickle as pkl
+
+import matplotlib.pyplot as plt
+
+from scipy import stats
+from utilities import *
+from models import *
+from sklearn import preprocessing
+from shapreg import removal, games, shapley
+
+
+#### import CATE model
+module_path = os.path.abspath(os.path.join('CATENets/'))
+
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+from catenets.models.jax import TNet, SNet,SNet1, SNet2, SNet3, DRNet, RANet, PWNet, RNet, XNet
+import catenets.models as cate_models
+
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser(description='Description of your program')
+    parser.add_argument('-d','--d', help='feature dimension', required=True)
+    parser.add_argument('-n','--n', help='sample size',required=True)
+    parser.add_argument('-e','--e', help='training epoches',required=True)
+    parser.add_argument('-r','--r', help='random state',required=True)
+
+    args = vars(parser.parse_args())
+    
+    n = int(args["n"])
+    feature_size = int(args["d"])
+    random_state = int(args["r"])
+    epoches = int(args["e"])
+    device = "cuda:0"
+    #path = ("results_d=%s_n=%s_r=%s/"%(feature_size, n, random_state))
+    
+    #if not os.path.exists(path):
+    #    os.makedirs(path)
+    
+    X, y_po, w, p, KL = simulation(feature_size, n, 0, 0, random_state, False)
+
+    min_max_scaler = preprocessing.MinMaxScaler()
+    X_scaled = min_max_scaler.fit_transform(X)
+
+    #print(KL)
+    rng = np.random.default_rng(random_state)
+    inds = np.arange(n)
+    rng.shuffle(inds)
+
+    n_train = int(0.8 * n)
+
+    train_inds = inds[:n_train]
+    test_inds = inds[n_train:]
+
+    x_oracle_train = torch.from_numpy(X_scaled[train_inds,:]).to(device)
+    w_oracle_train = torch.from_numpy(w[train_inds,:]).to(device)
+    y_oracle_train = torch.from_numpy(np.take_along_axis(y_po,w, 1)[train_inds, :]).to(device)
+    y_test_cate = y_po[test_inds, 1] - y_po[test_inds, 0]
+
+    ### obtain oracle with DRNet()
+    print("training oracle.")
+    dr_unbiased, dr_unbiased_abs = oracle(X_scaled[train_inds,:], 
+                                        w[train_inds,:],
+                                        np.take_along_axis(y_po,w, 1)[train_inds, :], 
+                                        X_scaled[test_inds,:], 
+                                        DRNet(nonlin="relu"))
+    ### Create Cate model 
+
+    torch_DR = cate_models.torch.DRLearner(
+                2*feature_size,
+                binary_y=(len(np.unique(y_po)) == 2),
+                n_layers_out=2,
+                n_units_out=100,
+                n_iter=1,
+                batch_size=128,
+                batch_norm=False,
+                nonlin="relu",
+                ).to(device)
+    mask_layer = MaskLayer().to(device)
+    cate_model = Cate(torch_DR, mask_layer, device)
+
+    ### Train model with maskes
+
+    cate_model.fit(x_oracle_train, y_oracle_train, w_oracle_train, epoches)
+
+    x_oracle_test = torch.from_numpy(X_scaled[test_inds,:]).to(device)
+    w_oracle_test = torch.from_numpy(w[test_inds,:]).to(device)
+    test_mask = torch.ones(x_oracle_test.size()[0],x_oracle_test.size()[1]).to(device)
+    test_phe = cate_model.predict(x_oracle_test, test_mask).cpu().detach().numpy()
+
+    print("phe is %s" %mse(test_phe, y_test_cate))
+
+    #### Explanation method
+
+    test_values = np.zeros((len(test_inds) ,feature_size ))
+    
+    print("======explanation starts.=======")
+
+    for test_ind in range(x_oracle_test.size()[0]):
+        instance = torch.reshape(torch.from_numpy(X_scaled[test_ind, :]), (1,-1)).to(device)
+        game  = games.CateGame(instance, cate_model, device)
+        explanation = shapley.ShapleyRegression(game, batch_size=32)
+
+        test_values[test_ind] = explanation.values
+    
+    mask_shap = np.mean(test_values, axis=0)
+    mask_shap_abs = np.mean(np.abs(test_values), axis=0)
+
+    print("Final results.")
+    print(stats.spearmanr(dr_unbiased , mask_shap).correlation)
+    print(stats.spearmanr(dr_unbiased , mask_shap_abs).correlation)
